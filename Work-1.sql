114a83119bcb334fc6e06a85eeda567bd13aecd4
Gattamaneni venkata sashank <121079094+sashanka74@users.noreply.github.com>
2025-11-13 12:13:09 +0530

    Add files via upload

---
+++ Work-1.sql
@@ -0,0 +1,184 @@
+USE SqlPractice
+
+CREATE TABLE Sem1
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks bigint
+)
+
+SELECT * FROM Sem1
+
+INSERT INTO Sem1
+values
+(1,'English','A+','Pass'),
+(2,'Maths','C','Pass'),
+(3,'Physics','A','Pass'),
+(4,'Social','D','Pass'),
+(5,'AP','B','Pass')
+
+ALTER TABLE Sem1 ALTER column Sub_remarks varchar(50)
+
+CREATE TABLE Sem2
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks varchar(50)
+)
+
+SELECT * FROM Sem2
+
+INSERT INTO Sem2
+values
+(1,'ITVC','A+','Pass'),
+(2,'CA','C','Pass'),
+(3,'ENGLISH','A','Pass'),
+(4,'Python','D','Pass'),
+(5,'UHV','B','Pass')
+
+CREATE TABLE Sem3
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks bigint
+)
+
+SELECT * FROM Sem3
+
+INSERT INTO Sem3
+values
+(1,'English','A+','Pass'),
+(2,'Maths','C','Pass'),
+(3,'Physics','A','Pass'),
+(4,'Social','D','Pass'),
+(5,'AP','B','Pass')
+ALTER TABLE Sem3 ALTER column Sub_remarks varchar(50)
+
+CREATE TABLE Sem4
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks varchar(50)
+)
+
+SELECT * FROM Sem4
+
+INSERT INTO Sem4
+values
+(1,'ITVC','A+','Pass'),
+(2,'CA','C','Pass'),
+(3,'ENGLISH','A','Pass'),
+(4,'Python','D','Pass'),
+(5,'UHV','B','Pass')
+
+CREATE TABLE Sem5
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks varchar(50)
+)
+
+SELECT * FROM Sem5
+
+INSERT INTO Sem5
+values
+(1,'ITVC','A+','Pass'),
+(2,'CA','C','Pass'),
+(3,'ENGLISH','A','Pass'),
+(4,'Python','D','Pass'),
+(5,'UHV','B','Pass')
+
+CREATE TABLE Sem6
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks varchar(50)
+)
+
+SELECT * FROM Sem6
+
+INSERT INTO Sem6
+values
+(1,'ITVC','A+','Pass'),
+(2,'CA','C','Pass'),
+(3,'ENGLISH','A','Pass'),
+(4,'Python','D','Pass'),
+(5,'UHV','B','Pass')
+
+CREATE TABLE Sem7
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks varchar(50)
+)
+
+SELECT * FROM Sem7
+
+INSERT INTO Sem7
+values
+(1,'ITVC','A+','Pass'),
+(2,'CA','C','Pass'),
+(3,'ENGLISH','A','Pass'),
+(4,'Python','D','Pass'),
+(5,'UHV','B','Pass')
+
+CREATE TABLE Sem8
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks varchar(50)
+)
+
+SELECT * FROM Sem8
+
+INSERT INTO Sem8
+values
+(1,'ITVC','A+','Pass'),
+(2,'CA','C','Pass'),
+(3,'ENGLISH','A','Pass'),
+(4,'Python','D','Pass'),
+(5,'UHV','B','Pass')
+
+CREATE TABLE Sem9
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks varchar(50)
+)
+
+SELECT * FROM Sem9
+
+INSERT INTO Sem9
+values
+(1,'ITVC','A+','Pass'),
+(2,'CA','C','Pass'),
+(3,'ENGLISH','A','Pass'),
+(4,'Python','D','Pass'),
+(5,'UHV','B','Pass')
+
+CREATE TABLE Sem10
+(
+Sub_id int,
+Sub_name nvarchar(28),
+Sub_grade nvarchar(28),
+Sub_remarks varchar(50)
+)
+
+SELECT * FROM Sem10
+
+INSERT INTO Sem10
+values
+(1,'ITVC','A+','Pass'),
+(2,'CA','C','Pass'),
+(3,'ENGLISH','A','Pass'),
+(4,'Python','D','Pass'),
+(5,'UHV','B','Pass')
